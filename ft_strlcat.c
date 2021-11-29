3947fa52b1ccece5a1a059f7c55efb770f02ee47
LEECHANGHEE <changheejang@naver.com>
2021-11-29 18:24:49 +0900

    aa

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -14,18 +14,20 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
+	size_t	i;
 	size_t	dst_len;
 	size_t	src_len;
 
 	dst_len = ft_strlen(dst);
 	src_len = ft_strlen(src);
-	while (dstsize > dst_len + 1 && *src != '\0')
+	i = 0;
+	while (dstsize > dst_len + i + 1 && *src != '\0')
 	{
-		*(dst + dst_len) = *(char *)src;
-		dst++;
-		src++;
-		dstsize--;
+		dst[dst_len + i] = src[i];
+		i++;
 	}
-	*(dst + dst_len) = '\0';
+	dst[dst_len + 1] = '\0';
+	if (dstsize <= dst_len)
+		dst_len = dstsize;
 	return (dst_len + src_len);
 }
