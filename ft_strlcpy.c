95da9f8a2e736645ce8ab026316cec87b797e0ed
Changhee Lee <changhle@c7r1s5.42seoul.kr>
2021-12-05 20:41:25 +0900

    libft

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: changhle <changhle@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/30 01:22:22 by changhle          #+#    #+#             */
-/*   Updated: 2021/11/30 02:17:26 by changhle         ###   ########.fr       */
+/*   Updated: 2021/12/05 20:40:37 by changhle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,14 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	size_t	i;
 
 	i = 0;
-	while (src[i] != '\0' && i + 1 < dstsize)
-	{
-		dst[i] = src[i];
-		i++;
-	}
 	if (dstsize > 0)
+	{
+		while (src[i] != '\0' && i < dstsize - 1)
+		{
+			dst[i] = src[i];
+			i++;
+		}
 		dst[i] = '\0';
+	}
 	return (ft_strlen(src));
 }
