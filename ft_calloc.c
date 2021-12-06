f0cba6eb65bee710db81bc8a0ab39f84a0312d2d
Changhee Lee <changhle@c7r1s5.42seoul.kr>
2021-12-07 01:11:52 +0900

    libft

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: changhle <changhle@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/30 01:18:21 by changhle          #+#    #+#             */
-/*   Updated: 2021/12/06 18:39:33 by changhle         ###   ########.fr       */
+/*   Updated: 2021/12/07 00:35:51 by changhle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,8 @@
 
 void	*ft_calloc(size_t count, size_t size)
 {
-	size_t	i;
 	char	*arr;
 
-	if (count * size == 0)
-		return (NULL);
-	i = 0;
 	arr = malloc(count * size);
 	if (!arr)
 		return (arr);
