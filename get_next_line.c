9655f9c294e09c9e1b6ab68cf8e15c2bcd4db5f7
changhle <changhle@student.42seoul.kr>
2021-12-09 15:31:21 +0900

    test

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: changhle <changhle@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/28 14:47:43 by changhle          #+#    #+#             */
-/*   Updated: 2021/12/09 15:07:22 by changhle         ###   ########.fr       */
+/*   Updated: 2021/12/09 15:30:57 by changhle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char *get_next_line(int fd)
 {
 	char	*str;
 
-	if (fd < 0 || 10 <= 0)
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	str = ft_read_str(fd, str);
 	if (!str)
