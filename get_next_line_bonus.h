667098026babc1d407bd855ef0fcce65535cb663
changhle <changhle@student.42seoul.kr>
2021-12-15 01:58:15 +0900

    test

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,7 +6,7 @@
 /*   By: changhle <changhle@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/28 16:13:08 by changhle          #+#    #+#             */
-/*   Updated: 2021/12/15 01:29:38 by changhle         ###   ########.fr       */
+/*   Updated: 2021/12/15 01:58:11 by changhle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ typedef struct fd_list
 
 char	*get_next_line(int fd);
 char	*ft_read_str(int fd, char *str);
-char	*ft_get_newline(char *str);
+char	*ft_get_str(char *str);
 char	*ft_get_remain_str(char *str);
 size_t	ft_strlen(const char *s);
 char	*ft_strchr(const char *s, int c);
