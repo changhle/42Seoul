202f9474e69585ece93a64099b0b981080129873
changhle <changhle@student.42seoul.kr>
2023-02-06 05:16:46 +0900

    cpp00 ex00

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+void	ConvertToUpper(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+		i++;
+	}
+}
+
+int	main(int argc, char *argv[])
+{
+	int	i;
+
+	i = 1;
+	if (argc > 1)
+	{
+		while (i < argc)
+		{
+			ConvertToUpper(argv[i]);
+			std::cout << argv[i];
+			i++;
+		}
+		std::cout << std::endl;
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+}
---
+++ ex01/PhoneBook.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+class PhoneBook {
+	private:
+
+};
+
+class Contact {
+	private:
+		std::string	first_name;
+		std::string	last_name;
+		std::string	nickname;
+		std::string	phone_number;
+		std::string	darkest_secret;
+
+};
