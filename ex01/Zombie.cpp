395bd1e8c4e6474058418e14c948869d659ab2bc
changhle <changhle@student.42seoul.kr>
2023-02-23 10:26:01 +0900

    edited: ex01

--- ex01/Zombie.cpp
+++ ex01/Zombie.cpp
@@ -1,5 +1,10 @@
 #include "Zombie.hpp"
 
+Zombie::Zombie()
+{
+	std::cout << "Zombie is born..." << std::endl;
+}
+
 Zombie::Zombie(std::string name)
 {
 	this->name = name;
@@ -9,6 +14,11 @@ Zombie::Zombie(std::string name)
 void	Zombie::announce()
 {
 	std::cout << name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
+
+void	Zombie::setName(std::string name)
+{
+	this->name = name;
 }
 
 Zombie::~Zombie()
--- ex01/Zombie.hpp
+++ ex01/Zombie.hpp
@@ -8,8 +8,10 @@ class Zombie {
 		std::string	name;
 
 	public:
+		Zombie();
 		Zombie(std::string name);
 		void	announce();
+		void	setName(std::string name);
 		~Zombie();
 };
 
