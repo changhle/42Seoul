7f505c2ac043cc2c636eb53ec112063d7af1faac
Changhee Lee <changhle@cx2r6s1.42seoul.kr>
2023-03-07 18:52:47 +0900

    added: ex02

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -4,12 +4,24 @@
 #include <iostream>
 
 template <typename T>
-void	iter(const T *array, std::size_t len, void (*func)(T &))
+void	iter(T *array, std::size_t len, void (*func)(T &))
 {
 	for (std::size_t i = 0; i < len; i++)
 	{
 		func(array[i]);
 	}
+}
+
+template <typename T>
+void	print(T &arg)
+{
+	std::cout << arg << " ";
+}
+
+template <typename T>
+void	pow(T &arg)
+{
+	arg = arg * arg;
 }
 
 #endif
