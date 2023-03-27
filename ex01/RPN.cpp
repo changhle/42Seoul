75f856b1861a38e42e386223c87a1f774f5ae9b9
이창희 <changhee@changhles-MacBookPro.local>
2023-03-27 16:25:51 +0900

    added: ex01

---
+++ ex01/RPN.cpp
@@ -0,0 +1,58 @@
+#include "RPN.hpp"
+
+int	calculate(int operand_a, int operand_b, char oper)
+{
+	if (oper == '+')
+		return (operand_a + operand_b);
+	else if (oper == '-')
+		return (operand_a - operand_b);
+	else if (oper == '*')
+		return (operand_a * operand_b);
+	else
+		return (operand_a / operand_b);
+}
+
+void	RPN(char *expr)
+{
+	std::stack<int>	stack;
+	std::string		oper = "+-*/";
+	int				operand_a;
+	int				operand_b;
+	int				i;
+
+	i = 0;
+	while (expr[i])
+	{
+		if (expr[i] >= '0' && expr[i] <= '9')
+			stack.push(expr[i] - '0');
+		else if (oper.find(expr[i]) != std::string::npos)
+		{
+			if (stack.size() >= 2)
+			{
+				operand_b = stack.top();
+				stack.pop();
+				operand_a = stack.top();
+				stack.pop();
+				stack.push(calculate(operand_a, operand_b, expr[i]));
+			}
+			else
+			{
+				std::cerr << "Error" << std::endl;
+				return;
+			}
+		}
+		else if (expr[i] != ' ')
+		{
+			std::cerr << "Error" << std::endl;
+			return;
+		}
+		i++;
+	}
+	if (stack.size() == 1)
+		std::cout << stack.top() << std::endl;
+	else
+	{
+		std::cerr << "Error" << std::endl;
+		return;
+	}
+}
---
+++ ex01/RPN.hpp
@@ -0,0 +1,33 @@
+#ifndef RPN_HPP
+#define RPN_HPP
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <stack>
+
+void	RPN(char *expr);
+// class RPN {
+// 	private:
+// 	public:
+// 		RPN();
+// 		RPN(const RPN& obj);
+// 		~RPN();
+// 		RPN& operator=(const RPN& obj);
+// };
+
+#endif
+
+// RPN::RPN() {
+// 	std::cout << "[RPN] Default constructor" << '\n';
+// }
+// RPN::RPN(const RPN& obj) {
+// 	std::cout << "[RPN] Copy constructor" << '\n';
+// }
+// RPN::~RPN() {
+// 	std::cout << "[RPN] Destructor" << '\n';
+// }
+// RPN& RPN::operator=(const RPN& obj) {
+// 	std::cout << "[RPN] Copy assignment operator" << '\n';
+// 	return (*this);
+// }
